d8d8daaed67c27d6bac4bc5e7ca4223036501c56
Robert Sajdak <robert.sajdak@alusalon.com>
2019-12-23 22:09:35 +0100

    the last commit

--- src/main/java/pl/kodu/akademia/command/BmiResults.java
+++ src/main/java/pl/kodu/akademia/command/BmiResults.java
@@ -1,11 +1,18 @@
 package pl.kodu.akademia.command;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class BmiResults {
 
     private int weight;
     private int height;
     private double bmi;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
     public BmiResults() {
